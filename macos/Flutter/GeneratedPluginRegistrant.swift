73e6947855bf7ce2c59fa563110a4a30a32882d2
tmox <108318618+tm-ox@users.noreply.github.com>
2026-07-14 12:19:07 +0700

    Register package_info_plus in the macOS plugin registrant (#227)
    Generated file regenerated by flutter pub add; missed in #226. CI regenerates it on build, but keeping it in sync avoids a dirty tree.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
+import package_info_plus
 import share_plus
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
